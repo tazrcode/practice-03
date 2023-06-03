393274cab8dccabeeb0bdce03ed8580582dd095a
tawal zambrano <tazr.codes@gmail.com>
2023-06-03 16:38:45 -0400

    plantilla completada

--- script.js
+++ script.js
@@ -24,10 +24,17 @@ window.onscroll = () => {
              navLinks.forEach(links => {
                 links.classList.remove('active')
                 document.querySelector('header nav a[href*= '+ id +' ]').classList.add ('active');
-             }); 
-         }
-     })
+             });
+            //  active sections for animation on scroll
+            sec.classList.add('show-animate');
 
+        }
+        // if want to use animation that repeats on scroll use this 
+        else {
+            sec.classList.remove('show-animate');
+
+        }
+     })
 
     //sicky header
     let header = document.querySelector('header');
@@ -37,6 +44,11 @@ window.onscroll = () => {
     // remote toggle icon and navbar when click navbar links (scroll)
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
+
+    // Animation footer on scroll
+    let footer = document.querySelector('footer');
+
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 
 }
 
